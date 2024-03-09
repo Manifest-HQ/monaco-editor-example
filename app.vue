0de5e79c439db67ad45c50f5a542c3c466251349
Eduardo López <loama18@gmail.com>
2024-03-09 12:18:07 +0100

    editor working

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="background-color: #202020; padding: 24px 16px">
+    <MonacoEditor
+      v-model="value"
+      lang="javascript"
+      :options="{
+        theme: 'vs-dark'
+      }"
+      style="height: 100vh;" />
+  </div>
+</template>
+
+<script setup>
+const value = ref(`const a = 1
+const b = 2
+const c = 3
+
+function sum() {
+  return a + b + c
+}
+
+console.log(sum())`)
+</script>
+
+<style lang="sass">
+html, body
+  margin: 0
+</style>
