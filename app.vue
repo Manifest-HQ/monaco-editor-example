f990941c7eb02bdf2cb0c6f703452cc552e50e67
Eduardo López <loama18@gmail.com>
2024-03-20 11:19:52 +0100

    add linting

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div style="background-color: #202020; padding: 24px 16px">
     <MonacoEditor
       v-model="value"
-      lang="javascript"
+      lang="prisma"
       :options="{
         theme: 'vs-dark'
       }"
@@ -20,6 +20,36 @@ function sum() {
 }
 
 console.log(sum())`)
+
+onMounted(() => {
+  const monaco = useMonaco()
+
+  monaco.languages.register({ id: 'prisma' })
+  monaco.languages.setMonarchTokensProvider('prisma', {
+    tokenizer: {
+        root: [
+            // Keywords
+            [/\b(?:model|datasource|generator|type|enum)\b/, 'keyword'],
+            // Types
+            [/\b(?:String|Int|Boolean|DateTime|Float)\b/, 'type'],
+            // Comments
+            [/#.*$/, 'comment'], // hash style comments
+            [/\/\/.*$/, 'comment'], // double-slash comments
+            // Strings
+            [/"(?:[^"\\]|\\.)*"/, 'string'], // double quoted strings
+            [/'(?:[^'\\]|\\.)*'/, 'string'], // single quoted strings
+            // Identifiers
+            [/\b[a-zA-Z_]\w*\b/, 'identifier'],
+            // Brackets and Operators
+            [/[{}()\[\]]/, '@brackets'],
+            [/[,]/, 'delimiter'],
+            [/=/, 'operator'],
+            // For simplicity, let's remove the problematic pattern and not focus on < and > symbols for now.
+            // If needed, specific patterns for operators or specific symbols can be added separately.
+        ],
+    }
+  })
+})
 </script>
 
 <style lang="sass">
